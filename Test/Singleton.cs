9cb6f324020821e1a27db5367e07c37c1b13c800
sesamefly <sesamefly@163.com>
2018-06-07 14:19:44 +0800

    init

---
+++ Test/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    /// <summary>
+    /// 单例模式
+    /// 参考网址：https://www.jianshu.com/p/3ae1bd656c1f
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton single;
+        private  Singleton()
+        {
+            single = new Singleton();
+        }
+    }
+}
